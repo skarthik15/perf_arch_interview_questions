44a5a01b7de57ad9b0e1c43dcf13c39c752f0e39
Karthik <karthik10.shankar@gmail.com>
2022-10-29 13:15:58 -0700

    Initial updates

--- cache_replacement_policies/LRU/LRU.cpp
+++ cache_replacement_policies/LRU/LRU.cpp
@@ -1,1 +1,20 @@
 //LRU Implementation source file
+
+#include <iostream>
+#include <stdlib>
+#include <vector>
+#include "LRU.hpp"
+
+using namespace std;
+
+int update_lru(uint64_t input_tag, char op){
+	
+	
+
+}
+
+
+
+
+
+
--- cache_replacement_policies/LRU/LRU.hpp
+++ cache_replacement_policies/LRU/LRU.hpp
@@ -1,1 +1,15 @@
 //LRU Implementation header file
+//
+using namespace std;
+
+struct lru_meta_t {
+	uint64_t tag,
+	uint64_t dirty,
+	uint64_t valid,
+};
+
+std::vector<lru_meta_t> lru_list;
+
+
+int update_lru(uint64_t input_tag, char op );
+
